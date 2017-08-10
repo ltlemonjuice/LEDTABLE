5beecaa686baf26eb265e4d24d229059348c2401
ltlemonjuice <luca.fluri4@gmail.com>
2017-08-10 13:59:46 +0200

    fixed save.matrix writing

--- progs10/tetris.py
+++ progs10/tetris.py
@@ -278,10 +278,11 @@ def display():
 	try:
 	    spidev.flush()
 	except:
-	    pass
+        f = open("save.matrix", "r")
+        if not f.read()==numpy.array2string(numpy.flipud(numpy.rot90(matrix)), separator=", "):
+            f = open("save.matrix", "w+")
+			f.write(numpy.array2string(numpy.flipud(numpy.rot90(matrix)), separator=", "))
 
-	f = open("save.matrix", "w+")
-	f.write(numpy.array2string(numpy.flipud(numpy.rot90(matrix)), separator=", "))
 
 def clearMatrix():
 	for x in range(0, 10):
